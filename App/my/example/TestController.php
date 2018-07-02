d51a6e01504c10ffdcb99841d279f9478ce36ef6
Вячеслав <slavik@itservices.in.ua>
2018-07-02 15:30:22 +0300

    template fix

--- App/my/example/TestController.php
+++ App/my/example/TestController.php
@@ -18,13 +18,64 @@ class TestController extends Controller
 
             $this->response->setContent($template
                 ->render('index', [
-                'title' => 'Centurion app',
-                'header' => $template->render('component/header', [
-                    'auth'=>$template->render('component/header/auth'),
-                    'LogoSmall'=>'<b>app</b>',
-                    'LogoBig'=>'<b>Centurion</b>App',
-                    'dropdown'=>[['fa_icon'=>'fa fa-cloud-download', 'label'=>'new!' , /*'label_type'=>'warning' ,*/ 'header'=>'hello world', 'content'=>'<a href="/admin">Click this</a>' , 'footer'=> '<a href="#">See All Messages</a>']]
-                ])
+                    'title' => 'Centurion app',
+                    'sidebar'=>$template->render('component/sidebar' , [
+                        'section' => $template->render('component/sidebar/section', [
+                            'section' => [[
+                                'name' => 'Раздел разработки',
+                                'collection' => [
+                                    $template->render('component/sidebar/item', [
+                                        'fa_icon'=>'fa fa-laptop',
+                                        'name'=>'TEst item!',
+                                        'href'=>'/admin',
+                                        'badges'=>[
+                                            ['color'=>'green' , 'value'=>'yes!']
+                                        ]
+                                    ])
+                                ]
+                            ],
+                                [
+                                    'name' => 'Раздел разработки',
+                                    'collection' => [
+                                        $template->render('component/sidebar/item', [
+                                            'fa_icon'=>'fa fa-laptop',
+                                            'name'=>'TEst item!',
+                                            'href'=>'/admin',
+                                            'badges'=>[
+                                                ['color'=>'green' , 'value'=>'yes!']
+                                            ],
+                                            'child'=>[
+                                                $template->render('component/sidebar/item', [
+                                                    'fa_icon'=>'fa fa-laptop',
+                                                    'name'=>'TEst item!',
+                                                    'href'=>'/admin',
+                                                    'badges'=>[
+                                                        ['color'=>'green' , 'value'=>'yes!']
+                                                    ],
+                                                    'child'=>[
+                                                        $template->render('component/sidebar/item', [
+                                                            'fa_icon'=>'fa fa-laptop',
+                                                            'name'=>'TEst item!',
+                                                            'href'=>'/admin',
+                                                            'badges'=>[
+                                                                ['color'=>'green' , 'value'=>'yes!']
+                                                            ]
+                                                            ])
+                                                    ]
+                                                ])
+                                            ]
+                                        ])
+                                    ]
+                                ]
+                            ]
+                        ])
+                    ]),
+                    'header' => $template->render('component/header', [
+                        'auth'=>$template->render('component/header/auth'),
+                        'LogoSmall'=>'<b>app</b>',
+                        'LogoBig'=>'<b>Centurion</b>App',
+                        'dropdown'=>[['fa_icon'=>'fa fa-cloud-download', 'label'=>'new!' , 'label_type'=>'warning' , 'header'=>'hello world', 'content'=>'<a href="/admin">Click this</a>' , 'footer'=> '<a href="#">See All Messages</a>']]
+                    ])
             ]));
             //$this->response->setContent('<html><body><h1>Hello world.'.$this->arg['trans'].'</h1><a href="'.Router::url($this->route , ['trans'=>'sergey']).'">this route - '.$this->route.'</a></body></html>'.);
             $this->response->setStatusCode(Response::HTTP_OK);
